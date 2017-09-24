c6092e874cd4b79e917ef78e7cb3984ad1b5a0ae
vikushaan <vagevorkyan@edu.hse.ru>
2017-09-24 11:49:07 +0300

    hw-1

---
+++ HW-1/hw-1.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if a + b == c:
+    print ("Обладает свойством 1")
+else:
+    print ("Не обладает свойством 1")
+if a/b ==c:
+    print ("Обладает свойством 5")
+else:
+    print ("Не обладает свойством 5")
