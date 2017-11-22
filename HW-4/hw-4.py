28e9035e9885c145636196b76604d2d614ae0696
vikushaan <31884486+vikushaan@users.noreply.github.com>
2017-11-22 16:07:13 +0300

    Update hw-4.py

--- HW-4/hw-4.py
+++ HW-4/hw-4.py
@@ -1,18 +1,18 @@
 with open("text.txt", encoding="utf-8") as f:
    text = f.read()
 splited_text = text.split()
-wt = 0
-t = 0
+a = 0
+b = 0
 
 for i in enumerate(splited_text, start=1):
     if i[1] == '-':
-        t += 1
-    al = i[0] - t
+        b += 1
+    al = i[0] - b
 
     let = list(i[1])
     if let[-1] != '.' and let[-1] != ',' and i[1] != '-':
-        wt += 1
-p = 100 * wt / al
+        a += 1
+p = 100 * a / al
 print("Процент от общего количества слов не оканчивается знаком препинания = ", round(p, 2), "%")
 
     
