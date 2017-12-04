0ad3274f4dd4b87d8584f092033a5a79e62544e0
vikushaan <31884486+vikushaan@users.noreply.github.com>
2017-12-04 23:16:12 +0300

    Add files via upload

--- HW-5/hw-5.py
+++ HW-5/hw-5.py
@@ -1,14 +1,14 @@
 with open("text.txt", "a", encoding="utf-8") as f: 
      print("Введите латинские слова.") 
-     a=input() 
-     if a=="": 
-      print("Вы ничего не ввели.") 
+     w=input() 
+     if wgfds=="": 
+      print("Вы ничего не ввели.")
      else: 
-      while a!="": 
-         if a.endswith("re"): 
-             f.write(a) 
+      while w!="": 
+         if w.endswith("re"): 
+             f.write(w) 
              f.write("\n")
-         if a.endswith("ri"):
-             f.write(a)
+         if w.endswith("ri"):
+             f.write(w)
              f.write("\n")
-         a=input()
+         w=input()
